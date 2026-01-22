46ba8667b01636c9132479b5d8594bbdd0ed39c0
NombreUsuario <correo@gmail.com>
2026-01-22 09:53:55 +0100

    cambio en enviroment.prod.ts para poder conectar con el movil

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiKey: '1a0ff574fc3e7f4f0334ad7310c8753b',
+  weatherApiUrl: 'https://api.openweathermap.org/data/2.5'
 };
