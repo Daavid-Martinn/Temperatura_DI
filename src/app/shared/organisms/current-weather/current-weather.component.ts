30faf3c4fc85c96d72d9f2d3d1f219233b5f9b8d
NombreUsuario <correo@gmail.com>
2026-01-22 09:40:33 +0100

    ultimos cambios, ya finalizado

--- src/app/shared/organisms/current-weather/current-weather.component.ts
+++ src/app/shared/organisms/current-weather/current-weather.component.ts
@@ -2,25 +2,24 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { CurrentWeather } from 'src/app/core/interfaces/weather-data';
-
-// 1. IMPORTAR ICONOS
 import { addIcons } from 'ionicons';
 import { paperPlaneOutline, rainyOutline, sunnyOutline, waterOutline, thermometerOutline } from 'ionicons/icons';
+
+// 1. IMPORTAR EL MÓDULO DE TRADUCCIÓN
+import { TranslateModule } from '@ngx-translate/core'; 
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule]
+  // 2. AÑADIRLO AQUÍ A LOS IMPORTS
+  imports: [CommonModule, IonicModule, TranslateModule] 
 })
 export class CurrentWeatherComponent {
-
   @Input() current!: CurrentWeather;
 
   constructor() { 
-    // 2. REGISTRARLOS
     addIcons({ paperPlaneOutline, rainyOutline, sunnyOutline, waterOutline, thermometerOutline });
   }
-
 }
